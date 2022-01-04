c262886bd39a72011b13f659c3ef2283eedf02b6
KhasanDev <63578294+hasanscsds@users.noreply.github.com>
2022-01-04 11:51:16 +0500

    vb init

---
+++ first variant/js/main.js
@@ -0,0 +1,11 @@
+
+$('.carousel_cards').slick({
+  dots: true,
+  arrows: false,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  draggable: true,
+  easing: 'linear',
+  speed: 1150,
+});
+
---
+++ second variant/js/main.js
@@ -0,0 +1,17 @@
+/** ==========================================================================================
+
+  Project :   FAdos.uz - Tools Store Ecommerce Html Template
+  Author :    Themetechmount
+  Author URI: http://www.themetechmount.com
+
+ =============== */
+ $('.carousel_cards').slick({
+  dots: true,
+  arrows: false,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  draggable: true,
+  easing: 'linear',
+  speed: 1150,
+});
+
